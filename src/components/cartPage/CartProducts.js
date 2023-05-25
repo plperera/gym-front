8a20cb9a2cee292c54e2832fc9ff7985f775127d
Pedro Pereira <plp.leoncio@gmail.com>
2023-05-25 19:08:07 -0300

    .

--- src/components/cartPage/CartProducts.js
+++ src/components/cartPage/CartProducts.js
@@ -18,6 +18,7 @@ export default function CartProducts({ setUserData, userData, products, setRefre
                 updatedProducts[i].amount -= 1; 
             }
         }
+        console.log(updatedProducts)
         setUserData({ ...userData, cart: updatedProducts });
     }
     function removeProduct(index) {
@@ -42,16 +43,16 @@ export default function CartProducts({ setUserData, userData, products, setRefre
                 <ProductContainer willDelete={i === timeDelete} key={i}>
 
                     <LeftSideProduct>
-                        <img src={`https://drive.google.com/uc?export=view&id=${userData?.cart[i]?.productData?.imagensProduto[0]?.imageRef}`} alt={userData.cart[i].productData.nome} />
+                        <img src={`https://drive.google.com/uc?export=view&id=${userData?.cart[i]?.productData?.imagensProduto[0]?.imageRef | ""}`} alt={userData.cart[i].productData.nome} />
                     </LeftSideProduct>
 
                     <RightSideProduct>
-                        <ProductTitle>{userData.cart[i].productData.nome}</ProductTitle>
+                        <ProductTitle>{userData.cart[i].productData.nome | ""}</ProductTitle>
                         <ProductAmountManager>
 
                             <ProductAmout>
                                 <div onClick={() => updateAmount("-",i)}>-</div>
-                                <Amount>{userData?.cart[i]?.amount}</Amount>
+                                <Amount>{userData?.cart[i]?.amount | ""}</Amount>
                                 <div onClick={() => updateAmount("+",i)}>+</div>
                             </ProductAmout>
 
@@ -59,8 +60,7 @@ export default function CartProducts({ setUserData, userData, products, setRefre
                         </ProductAmountManager>
                     </RightSideProduct>
                 </ProductContainer>
-            )
-            }
+            )}
         </Container>      
     )
 }
